0638ba5a41b8e6350ed1c519ea93d70f7a36f8d3
PvdBerg1998 <PvdBerg1998@users.noreply.github.com>
2025-05-13 09:59:17 +0200

    Improve API, fix non slice CC export

--- abreast/__init__.py
+++ abreast/__init__.py
@@ -1,2 +1,10 @@
-from .abreast import Abreast
-from .export_functions import export_points, export_vox
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 14:14:10 2021
+
+@author: Marta Pinto
+@author: Koen Michielsen
+@author: Pim van den Berg
+"""
+
+from abreast.cc import *
